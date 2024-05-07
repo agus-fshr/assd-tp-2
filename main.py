856e9778087d925b00cc9e5d0f25c204e07c0dc4
Agus Casas <agcasas@itba.edu.ar>
2024-05-07 00:03:15 -0300

    quiero pullear

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from frontend.pages.TestPage import *
 from frontend.pages.FilesPage import *
 from frontend.pages.TracksPage import *
 from frontend.pages.ExamplePage import *
+from frontend.pages.ReadMidiPage import *
 from frontend.pages.InstrumentPage import *
 from frontend.pages.SoundPlayerPage import *
 from frontend.pages.TracksMetadataPage import *
@@ -29,6 +30,7 @@ if __name__ == '__main__':
         TracksMetadataPage(),
         ExamplePage(),
         TestPage(),
+        ReadMidiPage(),
     ]
     ex = MainWindow(pages=pages, model=mainModel)
 
