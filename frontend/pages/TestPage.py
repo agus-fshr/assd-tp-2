d6ba78731c74b75c88a5adaafe0e2deea14f981b
Rafdal <rdalzotto@itba.edu.ar>
2024-05-07 11:18:48 -0300

    fix TestPage

--- frontend/pages/TestPage.py
+++ frontend/pages/TestPage.py
@@ -19,12 +19,6 @@ class TestPage(BaseClassPage):
         leftVLayout = QVBoxLayout()
 
         self.params = ParameterList(
-            NumParam('k', interval=(0, 10), value=2.0, step=0.01, text="k"),
-            NumParam('A', interval=(0, 10), value=0.1, step=0.01, text="Attack"),
-            NumParam('D', interval=(0, 10), value=0.1, step=0.01, text="Decay"),
-            NumParam('R', interval=(0, 10), value=0.3, step=0.01, text="Release"),
-            NumParam('dur', interval=(0, 10), value=1.0, step=0.01, text="Total Duration"),
-
             # TextParam("eq", value="a*cos(wa*x)+b*cos(wb*x)+c*cos(wc*x)", text="Equation"),
             # NumParam("a", interval=(0, 5), value=2, step=0.01, text="a = Amplitude"),
             # NumParam("b", interval=(0, 5), value=0.8, step=0.01, text="b = Amplitude"),
@@ -55,20 +49,6 @@ class TestPage(BaseClassPage):
 
     def on_edit(self, k=None, v=None):
 
-        # func = self.params.getFunction(eq="eq", var="x")
-        k = self.params["k"]
-        a = self.params["A"]
-        d = self.params["D"]
-        r = self.params["R"]
-        duration = self.params["dur"]
-
-        if duration < a + d:
-            duration = a + d
-
-        adsr = LinearADSR(k, a, d, r, duration)
-
-        y = adsr()
-        t = adsr.time()
+        # self.plotWidget.plot(x, y)
         
-        # y = y.astype(int)
-        self.plotWidget.plot(t, y)
+        pass
